8da799d3a0325d1c79eb2f5bece053fc1a600a45
xLasercut <tianxun.wang2004@gmail.com>
2024-12-08 11:24:03 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# ZSH arch linux bootstrapping
+
+
+## Install
+
+```shell
+curl -s -S -L https://raw.githubusercontent.com/xLasercut/my-zsh/master/bootstrap.sh | bash
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -11,11 +11,14 @@ sudo pacman -Syy
 sudo pacman -S --needed base-devel openssl zlib xz tk
 sudo pacman -S zsh
 
+mkdir -p ~/.config/systemd/user/
+
 git clone https://github.com/zsh-users/antigen.git ~/antigen
 
 curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/.zshrc" --output "${HOME}/.zshrc"
 curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/.p10k.zsh" --output "${HOME}/.p10k.zsh"
 curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/.zprofile" --output "${HOME}/.zprofile"
+curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/ssh-agent.service" --output "${HOME}/.config/systemd/user/ssh-agent.service"
 
 chsh -s /bin/zsh
 
