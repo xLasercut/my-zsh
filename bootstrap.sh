840edee4ff80a9603a6ff43844147f76fe148602
xLasercut <tianxun.wang2004@gmail.com>
2024-12-08 11:34:09 +0000

    updated order

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,10 +9,6 @@ if [[ ${current_user} == "root" ]]; then
   exit 1
 fi
 
-sudo pacman -Syy
-sudo pacman -S --noconfirm --needed base-devel openssl zlib xz tk
-sudo pacman -S --noconfirm zsh
-
 mkdir -p ~/.config/systemd/user/
 
 git clone https://github.com/zsh-users/antigen.git ~/antigen
@@ -21,6 +17,10 @@ curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/.zshrc" --output
 curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/.p10k.zsh" --output "${HOME}/.p10k.zsh"
 curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/.zprofile" --output "${HOME}/.zprofile"
 curl "https://raw.githubusercontent.com/xLasercut/my-zsh/master/ssh-agent.service" --output "${HOME}/.config/systemd/user/ssh-agent.service"
+
+sudo pacman -Syy
+sudo pacman -S --noconfirm --needed base-devel openssl zlib xz tk
+sudo pacman -S --noconfirm zsh
 
 chsh -s /bin/zsh
 
